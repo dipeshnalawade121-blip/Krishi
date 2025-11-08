b7a2878300d09b85e87b74738c688150b8bc20e5
dipeshnalawade121-blip <dipeshnalawade121@gmail.com>
2025-11-08 13:10:20 +0530

    Delete shop-profile/shop-profile-liquid-glass directory

--- shop-profile/shop-profile-liquid-glass/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initGlass } from './glass.js';
-import { initUI } from './ui.js';
-
-window.addEventListener('load', async () => {
-  // Start subtle glass animation first (behind everything)
-  await initGlass();
-  // Then boot your existing UI logic
-  await initUI();
-});
