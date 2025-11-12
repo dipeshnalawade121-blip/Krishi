3d25ff59012d6dc8eb955ec5ed43aec880c3826a
dipeshnalawade121-blip <dipeshnalawade121@gmail.com>
2025-11-12 16:10:05 +0530

    Update page.tsx

--- next/app/user-profile/page.tsx
+++ next/app/user-profile/page.tsx
@@ -689,7 +689,10 @@ const CompleteProfilePage: React.FC = () => {
                         id="google-link-btn"
                         ref={googleButtonContainerRef}
                         className="transition-opacity duration-300"
-                        style={{ opacity: isGoogleButtonReady ? 1 : 0 }}
+                        {/* style={{ opacity: isGoogleButtonReady ? 1 : 0 }} */}
+                        style={{ opacity: isGoogleButtonReady ? 1 : 0,
+                                height: '50px' // <-- THE FINAL CLS FIX: Force exact height
+                }}
                       />
                     </div>
                   </div>
